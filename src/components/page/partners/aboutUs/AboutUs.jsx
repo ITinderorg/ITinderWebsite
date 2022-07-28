b838d1382d39ad0e3a360d1cc75c9ece84a93f20
Vlady <vlady@onlinedata.dk>
2022-07-28 23:01:26 +0200

    Adjusted AboutUs page to code format

--- src/components/page/partners/aboutUs/AboutUs.jsx
+++ src/components/page/partners/aboutUs/AboutUs.jsx
@@ -1,27 +1,33 @@
 import classes from "./AboutUs.module.css";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const AboutUs = () => {
   return (
     <section className="section" id="aboutUs">
       <Container>
         <Row className="text-center">
-          <h1>Про нас</h1>
-        </Row>
-        <Row>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-            doloremque debitis, dolore eligendi quae molestias eos illo vero ab
-            enim, asperiores assumenda sequi modi iusto tenetur aliquam minima,
-            recusandae maiores! Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Natus doloremque debitis, dolore eligendi quae
-            molestias eos illo vero ab enim, asperiores assumenda sequi modi
-            iusto tenetur aliquam minima, recusandae maiores! Lorem ipsum dolor
-            sit amet consectetur adipisicing elit. Natus doloremque debitis,
-            dolore eligendi quae molestias eos illo vero ab enim, asperiores
-            assumenda sequi modi iusto tenetur aliquam minima, recusandae
-            maiores!
-          </p>
+          <Col lg={12}>
+            <div className="center-heading">
+              <h2 className="section-title">Про нас</h2>
+            </div>
+          </Col>
+          <Col lg={6} className="offset-lg-3">
+            <div className="center-text">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                doloremque debitis, dolore eligendi quae molestias eos illo vero
+                ab enim, asperiores assumenda sequi modi iusto tenetur aliquam
+                minima, recusandae maiores! Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Natus doloremque debitis, dolore
+                eligendi quae molestias eos illo vero ab enim, asperiores
+                assumenda sequi modi iusto tenetur aliquam minima, recusandae
+                maiores! Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Natus doloremque debitis, dolore eligendi quae molestias
+                eos illo vero ab enim, asperiores assumenda sequi modi iusto
+                tenetur aliquam minima, recusandae maiores!
+              </p>
+            </div>
+          </Col>
         </Row>
       </Container>
     </section>

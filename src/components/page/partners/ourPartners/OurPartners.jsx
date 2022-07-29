077129a552ef4f9d7d8ade035e2e10407dbb9c6a
Vlady <vlady@onlinedata.dk>
2022-07-29 16:43:34 +0200

    Made background on OurPartners section

--- src/components/page/partners/ourPartners/OurPartners.jsx
+++ src/components/page/partners/ourPartners/OurPartners.jsx
@@ -5,43 +5,45 @@ import constants from "../../../../constants/PartnersConstants";
 const OurPartners = () => {
   const partners = constants.Partners;
   return (
-    <section className="section" id="ourPartners">
-      <Container>
-        <Row>
-          <Col lg={12}>
-            <div className="center-heading">
-              <h2 className="section-title">Наші партнери</h2>
-            </div>
-          </Col>
-        </Row>
-        <Row className="padding-top-70">
-          {partners.map((item) => {
-            return (
-              <Col lg={3} md={6} sm={6} key={item.id}>
-                <div className="features-small-item">
-                  <div className="icon">
-                    <i>
-                      <img
-                        width="100"
-                        height="100"
-                        src={
-                          require("../../../../assets/images/" + item.imageName)
-                            .default.src
-                        }
-                        alt="telegram"
-                        title="telegram"
-                        loading="lazy"
-                      />
-                    </i>
+    <section className="mini" id="ourPartners">
+      <div className="mini-content">
+        <Container>
+          <Row>
+            <Col lg={12}>
+              <div className="center-heading info">
+                <h2>Наші партнери</h2>
+              </div>
+            </Col>
+          </Row>
+          <Row className="padding-top-70">
+            {partners.map((item) => {
+              return (
+                <Col lg={3} md={6} sm={6} key={item.id}>
+                  <div className="features-small-item">
+                    <div className="icon">
+                      <i>
+                        <img
+                          width="100"
+                          height="100"
+                          src={
+                            require("../../../../assets/images/" +
+                              item.imageName).default.src
+                          }
+                          alt="telegram"
+                          title="telegram"
+                          loading="lazy"
+                        />
+                      </i>
+                    </div>
+                    <h5 className="features-title">{item.name}</h5>
+                    <p>{item.description}</p>
                   </div>
-                  <h5 className="features-title">{item.name}</h5>
-                  <p>{item.description}</p>
-                </div>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
     </section>
   );
 };

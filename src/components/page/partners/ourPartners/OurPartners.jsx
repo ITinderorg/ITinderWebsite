4320bee08bf23f19b0044df7164e907a195f2121
Vlady <vlady@onlinedata.dk>
2022-08-28 20:10:01 +0200

    Added kitcode logo

--- src/components/page/partners/ourPartners/OurPartners.jsx
+++ src/components/page/partners/ourPartners/OurPartners.jsx
@@ -4,7 +4,7 @@ import constants from "../../../../constants/PartnersConstants";
 
 const OurPartners = () => {
   const partners = constants.Partners;
-  /*return (
+  return (
     <section className="mini" id="ourPartners">
       <div className="mini-content">
         <Container className="padding-top-40">
@@ -18,13 +18,19 @@ const OurPartners = () => {
           <Row className="padding-top-70">
             {partners.map((item) => {
               return (
-                <Col lg={3} md={6} sm={6} key={item.id}>
-                  <div className="features-small-item">
+                <Col
+                  lg={4}
+                  md={6}
+                  sm={6}
+                  key={item.id}
+                  className="offset-lg-4 offset-md-3 offset-sm-3 d-flex justify-content-center"
+                >
+                  <div className={classes.item}>
                     <div className="icon">
                       <i>
                         <img
-                          width="100"
-                          height="100"
+                          width="140"
+                          height="160"
                           src={
                             require("../../../../assets/images/" +
                               item.imageName).default.src
@@ -35,45 +41,10 @@ const OurPartners = () => {
                         />
                       </i>
                     </div>
-                    <h5 className="features-title">{item.name}</h5>
-                    <p>{item.description}</p>
                   </div>
                 </Col>
               );
             })}
-          </Row>
-        </Container>
-      </div>
-    </section>
-  );*/
-  return (
-    <section className="mini" id="ourPartners">
-      <div className="mini-content">
-        <Container className="padding-top-40">
-          <Row>
-            <Col lg={12}>
-              <div className="center-heading info">
-                <h2>Наші партнери</h2>
-              </div>
-            </Col>
-          </Row>
-          <Row className="padding-top-70">
-            <Col lg={12}>
-              <div className="center-heading info text-center">
-                <h2 className={classes.words}>
-                  <span className={classes.jump + " " + classes.one}>C</span>
-                  <span className={classes.jump + " " + classes.two}>o</span>
-                  <span className={classes.jump + " " + classes.three}>m</span>
-                  <span className={classes.jump + " " + classes.four}>i</span>
-                  <span className={classes.jump + " " + classes.five}>n</span>
-                  <span className={classes.jump + " " + classes.six}>g</span>
-                  <span className={classes.jump + " " + classes.seven}>s</span>
-                  <span className={classes.jump + " " + classes.eight}>o</span>
-                  <span className={classes.jump + " " + classes.nine}>o</span>
-                  <span className={classes.jump + " " + classes.ten}>n</span>
-                </h2>
-              </div>
-            </Col>
           </Row>
         </Container>
       </div>

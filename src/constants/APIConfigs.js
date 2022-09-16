5adbe667455a00acb79f5032a657559be86a347c
Vlady <vlady@onlinedata.dk>
2022-09-16 17:45:04 +0200

    Updated api url

--- src/constants/APIConfigs.js
+++ src/constants/APIConfigs.js
@@ -1,7 +1,7 @@
 const APIConfigs = {
   ITmentor: {
     endpoint:
-      "https://f160-2a02-a310-823a-8c80-b926-61b5-a421-da99.eu.ngrok.io",
+      "https://1c52-2a02-a310-823a-8c80-f87e-e850-9ef9-6bc3.eu.ngrok.io",
   },
   LiqPay: {
     publicKey: "sandbox_i30542783909",

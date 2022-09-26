28d49d238d5f884460841a7ead4bcbdaaec665e0
Vlady <vlady@onlinedata.dk>
2022-09-26 11:35:15 +0200

    Added export path map

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/ITpartners": { page: "/ITpartners" },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

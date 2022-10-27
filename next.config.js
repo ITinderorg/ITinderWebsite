142db63b40a48368410ec4e75cef8b20a54623bc
Vlady <vlady@onlinedata.dk>
2022-10-27 18:19:34 +0200

    Removed unnecessary configs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   swcMinify: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/ITpartners": { page: "/ITpartners" },
-    };
-  },
   /*i18n: {
     locales: ["ua"],
     defaultLocale: "ua",
